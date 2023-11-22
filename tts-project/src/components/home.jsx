8103b5f48f2f8e89cd119d115bfab034fa9c2505
fadhildaffa <mdaffadhil98@gmail.com>
2023-11-22 15:03:26 +0700

    changing home jsx

--- tts-project/src/components/home.jsx
+++ tts-project/src/components/home.jsx
@@ -2,57 +2,42 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 export default function Home() {
-  const [socket, setSocket] = useState(io('http://localhost:3000'));
-  const [microphone, setMicrophone] = useState(false);
-  const [online, setOnline] = useState(false);
+  const [socket, setSocket] = useState();
+  const [users, setUsers] = useState([]);
   const [userStatus, setUserStatus] = useState({
     microphone: false,
     mute: false,
-    username: 'user#' + Math.floor(Math.random() * 999999),
+    username: 'user#',
     online: false,
   });
 
   useEffect(() => {
-    // const s = ;
-    socket.emit('init', { userId: 1 });
-    // setSocket(s);
-  }, [socket]);
-
-  // window.onload = () => {
-  // mainFunction(1000);
-  // };
+    const s = io('http://localhost:3000');
+    s.emit('init', { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzAwNjI3MDIzfQ.t9pHFTDRqLDiHko3yUViKoE0Ah1o_B6NfZW0QLReODg' });
+    setSocket(s);
+  }, []);
 
   if (socket) {
     socket.emit('userInformation', userStatus);
   }
 
-  //   const userStatus = {
-  //     microphone: false,
-  //     mute: false,
-  //     username: 'user#' + Math.floor(Math.random() * 999999),
-  //     online: false,
-  //   };
-
   function toggleConnection(e) {
-    console.log(userStatus.online);
+    e.preventDefault();
     if (userStatus.online === false) {
       e.target.classList.remove('btn-primary');
       e.target.classList.add('btn-danger');
     } else {
-      console.log('tes');
       e.target.classList.remove('btn-danger');
       e.target.classList.add('btn-primary');
     }
-    // userStatus.online = !userStatus.online;
-    // console.log(!online);
-    setOnline(!online);
+
     setUserStatus({ ...userStatus, online: !userStatus.online });
 
-    // editButtonClass(e, userStatus.online);
     emitUserInformation();
   }
 
   function toggleMute(e) {
+    e.preventDefault();
     if (userStatus.mute === false) {
       e.target.classList.remove('btn-primary');
       e.target.classList.add('btn-danger');
@@ -60,19 +45,14 @@ export default function Home() {
       e.target.classList.remove('btn-danger');
       e.target.classList.add('btn-primary');
     }
-    // console.log(userStatus);
-    setMicrophone(!microphone);
 
     setUserStatus({ ...userStatus, mute: !userStatus.mute });
 
-    // userStatus.mute = !userStatus.mute;
-
-    // editButtonClass(e, userStatus.mute);
     emitUserInformation();
   }
 
   function toggleMicrophone(e) {
-    // console.log(userStatus);
+    e.preventDefault();
     if (userStatus.microphone === false) {
       e.target.classList.remove('btn-primary');
       e.target.classList.add('btn-danger');
@@ -85,16 +65,6 @@ export default function Home() {
 
     emitUserInformation();
   }
-
-  //   function editButtonClass(target, bool) {
-  //     const classList = target.classList;
-  //     classList.remove('enable-btn');
-  //     classList.remove('disable-btn');
-
-  //     if (bool) return classList.add('enable-btn');
-
-  //     classList.add('disable-btn');
-  //   }
 
   useEffect(() => {
     function mainFunction(time) {
@@ -115,13 +85,12 @@ export default function Home() {
 
           let fileReader = new FileReader();
           fileReader.readAsDataURL(audioBlob);
+          console.log(userStatus.microphone, userStatus.online);
+
           fileReader.onloadend = function () {
-            // console.log(userStatus.microphone);
             if (!userStatus.microphone || !userStatus.online) return;
-            // console.log('masuk');
 
             let base64String = fileReader.result;
-            // console.log('masuk sini gan', socket);
             socket.emit('voice', base64String);
           };
 
@@ -136,19 +105,29 @@ export default function Home() {
           mediaRecorder.stop();
         }, time);
       });
-
-      socket.on('send', function (data) {
-        let audio = new Audio(data);
-        audio.play();
+      if (userStatus.online === true) {
+        socket.on('send', function (data) {
+          let audio = new Audio(data);
+          audio.play();
+        });
+      }
+      socket.on('usersUpdate', function (data) {
+        const newUsers = [];
+        console.log(data);
+        for (const newUser in data) {
+          newUsers.push(newUser);
+        }
+        setUsers(newUsers);
       });
     }
-    mainFunction(1000);
-  }, [microphone, socket, userStatus.microphone, userStatus.online]);
+    if (socket) {
+      mainFunction(1000);
+    }
+  }, [socket, userStatus]);
 
   function emitUserInformation() {
     socket.emit('userInformation', userStatus);
   }
-  //   socket.emit('userInformation', userStatus);
 
   return (
     <>
@@ -177,12 +156,11 @@ export default function Home() {
           <div className="col bg-dark-subtle">
             <div className="d-flex justify-content-center py-3">
               <div className="d-flex flex-column ">
-                <h3 className="fw-bold">Online list User</h3>
+                <h3 className="fw-bold text-center">Online list User</h3>
                 <div className="container text-center">
-                  <h5 className="py-2">User1</h5>
-                  <h5 className="py-2">User1</h5>
-                  <h5 className="py-2">User1</h5>
-                  <h5 className="py-2">User1</h5>
+                  {users.map((user) => {
+                    return <h5>{user}</h5>;
+                  })}
                 </div>
               </div>
             </div>
