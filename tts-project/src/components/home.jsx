2c38efd14ed9c4942bfc8a5bc9a42dcc75e8eb79
benisprlh <benisaprulah5@gmail.com>
2023-11-22 13:59:38 +0700

    fix: bug fixing on connection button

--- tts-project/src/components/home.jsx
+++ tts-project/src/components/home.jsx
@@ -2,21 +2,21 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 export default function Home() {
-  const [socket, setSocket] = useState(io('http://localhost:3000'));
-  const [microphone, setMicrophone] = useState(false);
-  const [online, setOnline] = useState(false);
+  const [socket, setSocket] = useState();
+  // const [microphone, setMicrophone] = useState(false);
+  // const [online, setOnline] = useState(false);
   const [userStatus, setUserStatus] = useState({
     microphone: false,
     mute: false,
-    username: 'user#' + Math.floor(Math.random() * 999999),
+    username: 'user#',
     online: false,
   });
 
   useEffect(() => {
-    // const s = ;
-    socket.emit('init', { access_token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzAwNTczNTc3fQ.ld36gIV4CF7XjfMnOn8TijJe8ZyTRVoO7xZcPesslVc' });
-    // setSocket(s);
-  }, [socket]);
+    const s = io('http://localhost:3000');
+    s.emit('init', { access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzAwNjI3MDIzfQ.t9pHFTDRqLDiHko3yUViKoE0Ah1o_B6NfZW0QLReODg' });
+    setSocket(s);
+  }, []);
 
   // window.onload = () => {
   // mainFunction(1000);
@@ -34,18 +34,18 @@ export default function Home() {
   //   };
 
   function toggleConnection(e) {
-    console.log(userStatus.online);
+    e.preventDefault();
+    // console.log(userStatus.online);
     if (userStatus.online === false) {
       e.target.classList.remove('btn-primary');
       e.target.classList.add('btn-danger');
     } else {
-      console.log('tes');
       e.target.classList.remove('btn-danger');
       e.target.classList.add('btn-primary');
     }
     // userStatus.online = !userStatus.online;
     // console.log(!online);
-    setOnline(!online);
+    // setOnline(!online);
     setUserStatus({ ...userStatus, online: !userStatus.online });
 
     // editButtonClass(e, userStatus.online);
@@ -53,6 +53,7 @@ export default function Home() {
   }
 
   function toggleMute(e) {
+    e.preventDefault();
     if (userStatus.mute === false) {
       e.target.classList.remove('btn-primary');
       e.target.classList.add('btn-danger');
@@ -60,8 +61,6 @@ export default function Home() {
       e.target.classList.remove('btn-danger');
       e.target.classList.add('btn-primary');
     }
-    // console.log(userStatus);
-    setMicrophone(!microphone);
 
     setUserStatus({ ...userStatus, mute: !userStatus.mute });
 
@@ -72,6 +71,7 @@ export default function Home() {
   }
 
   function toggleMicrophone(e) {
+    e.preventDefault();
     // console.log(userStatus);
     if (userStatus.microphone === false) {
       e.target.classList.remove('btn-primary');
@@ -97,8 +97,10 @@ export default function Home() {
   //   }
 
   useEffect(() => {
+    console.log('123');
     function mainFunction(time) {
       navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+        console.log('tes');
         let mediaRecorder = new MediaRecorder(stream);
         mediaRecorder.start();
 
@@ -115,13 +117,12 @@ export default function Home() {
 
           let fileReader = new FileReader();
           fileReader.readAsDataURL(audioBlob);
+          console.log(userStatus.microphone, userStatus.online);
+
           fileReader.onloadend = function () {
-            // console.log(userStatus.microphone);
             if (!userStatus.microphone || !userStatus.online) return;
-            // console.log('masuk');
 
             let base64String = fileReader.result;
-            // console.log('masuk sini gan', socket);
             socket.emit('voice', base64String);
           };
 
@@ -136,19 +137,21 @@ export default function Home() {
           mediaRecorder.stop();
         }, time);
       });
-
-      socket.on('send', function (data) {
-        let audio = new Audio(data);
-        audio.play();
-      });
+      if (userStatus.online === true) {
+        socket.on('send', function (data) {
+          let audio = new Audio(data);
+          audio.play();
+        });
+      }
     }
-    mainFunction(1000);
-  }, [microphone, socket, userStatus.microphone, userStatus.online]);
+    if (socket) {
+      mainFunction(1000);
+    }
+  }, [socket, userStatus]);
 
   function emitUserInformation() {
     socket.emit('userInformation', userStatus);
   }
-  //   socket.emit('userInformation', userStatus);
 
   return (
     <>
